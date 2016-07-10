d9d9d07543af2dfdd2f49f3da39c1967ef59af61
Pravinth Samuel <Pravinth Samuel>
2016-07-10 23:53:34 +0530

    Added model to generate mask for training image

--- nervesegment.py
+++ nervesegment.py
@@ -15,6 +15,8 @@ from keras.layers import Convolution2D, Flatten, MaxPooling2D
 from keras.optimizers import SGD
 from keras.utils.np_utils import to_categorical
 
+import matplotlib.pyplot as plt
+
 imgShapeY = 420
 imgShapeX = 580
 reshapeFactor = 0.25
@@ -40,8 +42,8 @@ def loadTrainData():
         img = cv.resize(img, (imgRows, imgCols))
         trainSegs.append(img)        
         print (i, impath, mpath)
-        #if i>1000:
-        #    break
+        if i == 499:
+           break
         
     return (trainImgs, trainSegs)
     
@@ -73,17 +75,16 @@ def rleEncode(img):
 def isNotEmpty(listOfImgs):
     return [np.sum(img) > 10 for img in listOfImgs]
     
-def createKerasModel1():
+def createKerasModel_isMask():
     model = Sequential()
     
     model.add(Convolution2D(4, 3, 3, border_mode='same', init='glorot_uniform',
-                            input_shape=(1, imgRows, imgCols)))
+                            input_shape=(1, imgCols, imgRows)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.10))
     
-    model.add(Convolution2D(4, 3, 3, border_mode='same', init='glorot_uniform',
-                            input_shape=(1, imgRows, imgCols)))
+    model.add(Convolution2D(4, 3, 3, border_mode='same', init='glorot_uniform'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.10))
@@ -94,6 +95,94 @@ def createKerasModel1():
     
     sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(optimizer=sgd, loss='categorical_crossentropy')
+    
+    return model
+    
+def createKerasModel_whereMask():
+    model = Sequential()
+    
+    model.add(Convolution2D(4, 3, 3, border_mode='same', init='glorot_uniform',
+                            input_shape=(1, imgCols, imgRows)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.10))
+    
+    model.add(Convolution2D(4, 3, 3, border_mode='same', init='glorot_uniform'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.10))
+    
+    model.add(Convolution2D(4, 3, 3, border_mode='same', init='glorot_uniform'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.10))
+    
+    model.add(Convolution2D(4, 3, 3, border_mode='same', init='glorot_uniform'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.10))
+
+    # Final output layer    
+    model.add(Convolution2D(1, imgRows, imgCols, border_mode='same', init='glorot_uniform'))
+    model.add(Activation('sigmoid'))
+    
+    sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='mse')
+    
+    return model
+    
+def createKerasModel_whereMask2():
+    model = Sequential()
+    
+    model.add(Convolution2D(4, 3, 3, border_mode='same', init='glorot_uniform',
+                            input_shape=(1, imgCols, imgRows)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.10))
+    
+    model.add(Convolution2D(4, 3, 3, border_mode='same', init='glorot_uniform'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.10))
+    
+    model.add(Convolution2D(4, 3, 3, border_mode='same', init='glorot_uniform'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.10))
+    
+    model.add(Convolution2D(4, 3, 3, border_mode='same', init='glorot_uniform'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.10))
+    
+    model.add(Convolution2D(4, 3, 3, border_mode='same', init='glorot_uniform'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.10))
+    
+    model.add(Convolution2D(4, 3, 3, border_mode='same', init='glorot_uniform'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.10))
+    
+    model.add(Convolution2D(4, 3, 3, border_mode='same', init='glorot_uniform'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.10))
+    
+    model.add(Convolution2D(4, 3, 3, border_mode='same', init='glorot_uniform'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.10))
+    
+    model.add(Convolution2D(4, 3, 3, border_mode='same', init='glorot_uniform'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.10))
+    
+    model.add(Convolution2D(4, 3, 3, border_mode='same', init='glorot_uniform'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.10))
+    
+    model.add(Convolution2D(4, 3, 3, border_mode='same', init='glorot_uniform'))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.10))
+
+    # Final output layer    
+    model.add(Convolution2D(1, 3, 3, border_mode='same', init='glorot_uniform'))
+    model.add(Activation('sigmoid'))
+    
+    sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='binary_crossentropy')
     
     return model
     
@@ -116,28 +205,28 @@ if __name__ == "__main__":
     startTime = time.time()
     (trainImgs, trainSegs) = loadTrainData()
     loadTime = time.time() - startTime
-    
     print 'Took', loadTime, 'secs to load the training data'
-    
+    '''
     isMaskPresent = isNotEmpty(trainSegs)
     
     print 'Mask is present in', float(np.sum(isMaskPresent)*100)/float(len(isMaskPresent)), '% of the training data'
     
+    
     # Predict if mask is present
-    model1 = createKerasModel1()
+    model_isMask = createKerasModel_isMask()
     
 #    from keras.utils.visualize_util import plot
 #    plot(model1, to_file='model.png', show_shapes='True')
     trainImgs = normaliseImageArray(trainImgs)
     
-    trainImgs = np.array(trainImgs, dtype='uint8')
-    trainImgs = trainImgs.reshape(trainImgs.shape[0], 1, imgRows, imgCols)
+    trainImgs = np.array(trainImgs)
+    trainImgs = trainImgs.reshape(trainImgs.shape[0], 1, imgCols, imgRows)
     
     isMaskPresentCategorical = to_categorical(isMaskPresent, 2)
     
-    model1.fit(trainImgs, isMaskPresentCategorical, batch_size=100, nb_epoch=100, shuffle=True, verbose=1)
+    model_isMask.fit(trainImgs, isMaskPresentCategorical, batch_size=100, nb_epoch=100, shuffle=True, verbose=1)
     
-    trainPreds = model1.predict(trainImgs, batch_size=100, verbose=1)
+    trainPreds = model_isMask.predict(trainImgs, batch_size=100, verbose=1)
     
     isMaskPresentPred = [t[1]>t[0] for t in trainPreds]
     
@@ -147,10 +236,23 @@ if __name__ == "__main__":
             correctPreds += 1.0
     
     print 'Training accuracy is', correctPreds/len(isMaskPresent), '%'
+    '''
+    model_mask = createKerasModel_whereMask2()
+    from keras.utils.visualize_util import plot
+    plot(model_mask, to_file='model.png', show_shapes='True')
     
+    trainImgs = normaliseImageArray(trainImgs)
+    trainImgs = np.array(trainImgs)
+    #trainImgs = np.array(trainImgs, dtype='uint8')
+    trainImgs = trainImgs.reshape(trainImgs.shape[0], 1, imgCols, imgRows)
     
+    trainSegs = normaliseImageArray(trainSegs)
+    trainSegs = np.array(trainSegs)
+    trainSegs = trainSegs.reshape(trainSegs.shape[0], 1, imgCols, imgRows)
     
+    model_mask.fit(trainImgs, trainSegs, batch_size=100, nb_epoch=10, shuffle=True, verbose=1)
     
+    predMasks = model_mask.predict(trainImgs, batch_size=100, verbose=1)
     
-    
-
+    plt.imshow(predMasks[0][0], cmap='gray')  
+    # plt.imshow(trainSegs[0][0], cmap='gray')  
